8fd9847f2c7617a48d66aaa9dda10afa9e38edfc
Aditya Wirayudha <83379121+Adityawir0410@users.noreply.github.com>
2025-09-25 10:56:37 +0700

    first commit

---
+++ app/src/main/java/com/example/profilapp_235150207111067anakagungngurahadityawirayudha/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.profilapp_235150207111067anakagungngurahadityawirayudha
+
+import android.os.Bundle
+import android.widget.Space
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.profilapp_235150207111067anakagungngurahadityawirayudha.ui.theme.ProfilApp_235150207111067AnakAgungNgurahAdityaWirayudhaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ProfilApp_235150207111067AnakAgungNgurahAdityaWirayudhaTheme {
+                ProfilApp()
+            }
+        }
+    }
+}
+
+@Composable
+//@Preview
+fun ProfilApp () {
+    var isFollowed by rememberSaveable { mutableStateOf(false) }
+    Box(modifier = Modifier.fillMaxSize().background(Color.White), contentAlignment = Alignment.Center) {
+        Column {
+            Box(modifier = Modifier.fillMaxWidth().height(400.dp), contentAlignment = Alignment.Center){
+                Column (modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+                    Image(painterResource(R.drawable.mikir_kidz),
+                        contentDescription = "foto profile",
+                        modifier = Modifier
+                            .size(150.dp)
+                            .clip(CircleShape),
+                        contentScale = ContentScale.Crop
+                    )
+                    Spacer(modifier = Modifier.height(14.dp))
+                    Text("Anak Agung Ngurah Aditya Wirayudha", modifier = Modifier.padding(top = 14.dp))
+                    Text("235150207111067")
+                    Text("Mahasiswa Teknik Informatika")
+                    Spacer(modifier = Modifier.height(14.dp))
+                    OutlinedButton (onClick = {isFollowed = !isFollowed}) {
+                        Text(if(isFollowed)"unfollow" else "follow",
+                            color = Color.Gray)
+                    }
+                }
+            }
+        }
+    }
+}
