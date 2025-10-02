602d47f5bf9a35c694ec28bc3bccf446edff3fd1
Aditya Wirayudha <83379121+Adityawir0410@users.noreply.github.com>
2025-10-02 19:39:19 +0700

    BAB 3

--- app/src/main/java/com/example/profilapp_235150207111067anakagungngurahadityawirayudha/MainActivity.kt
+++ app/src/main/java/com/example/profilapp_235150207111067anakagungngurahadityawirayudha/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.profilapp_235150207111067anakagungngurahadityawirayudha
 
 import android.os.Bundle
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -17,15 +16,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -33,9 +29,12 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.profilapp_235150207111067anakagungngurahadityawirayudha.ui.theme.ProfilApp_235150207111067AnakAgungNgurahAdityaWirayudhaTheme
+import com.example.profilapp_235150207111067anakagungngurahadityawirayudha.bab3.CounterScreen
+import com.example.profilapp_235150207111067anakagungngurahadityawirayudha.bab3.ColorToggleScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,6 +42,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ProfilApp_235150207111067AnakAgungNgurahAdityaWirayudhaTheme {
+//                CounterScreen()
+//                ColorToggleScreen()
                 ProfilApp()
             }
         }
@@ -53,6 +54,7 @@ class MainActivity : ComponentActivity() {
 //@Preview
 fun ProfilApp () {
     var isFollowed by remember{ mutableStateOf(false) }
+//    var
     Box(modifier = Modifier.fillMaxSize().background(Color.DarkGray), contentAlignment = Alignment.Center) {
         Column {
             Box(modifier = Modifier.fillMaxWidth().height(400.dp), contentAlignment = Alignment.Center){
@@ -68,11 +70,13 @@ fun ProfilApp () {
                     Text("Anak Agung Ngurah Aditya Wirayudha", modifier = Modifier.padding(top = 14.dp), color = Color.White)
                     Text("235150207111067", color = Color.White)
                     Text("Mahasiswa Teknik Informatika", color = Color.White)
-                    Spacer(modifier = Modifier.height(14.dp))
+                    Spacer(modifier = Modifier.height(24.dp))
                     OutlinedButton (onClick = {isFollowed = !isFollowed}) {
                         Text(if(isFollowed)"unfollow" else "follow",
                             color = Color.White)
                     }
+                    Spacer(modifier = Modifier.height(24.dp))
+                    Text(if (isFollowed) "Anda Mengikuti Akun Ini" else "Anda Belum Mengikuti Aku Ini", style = TextStyle(fontSize = 24.sp), color = Color.White)
                 }
             }
         }
